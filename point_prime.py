ea847368fbd527ac7bb63ab813352ce922b97e67
albert_ga <97292142+albert-garreta@users.noreply.github.com>
2022-02-17 17:18:32 +0100

    Update point_prime.py

--- point_prime.py
+++ point_prime.py
@@ -6,17 +6,30 @@ class EllipticCurvePrime(object):
         self.A = A
         self.B = B
         self.p = p
+
+        # Aquí guardaremos todos los puntos de la curva
+        self.points = [EllipticPointPrime(None, None)]
         
-        # Aquí guardaremos todos los puntos de la curva 
-        # Es un conjunto en lugar de una lista para evitar
-        # puntos repetidos. Empezamos poniendo el punto del infinito
-        self.points = {EllipticPointPrime(None, None)}
+        print("\n\n================================\n"
+              f"Welcome to the elliptic curve {self}\n")
         
+    def __repr__(self):
+        return f"E: Y^2 = X^3 + {self.A}*X + {self.B} over the prime {self.p}"
+
     def find_all_points(self):
         """Encuentra todos los puntos de la curva elíptica"""
+        print("Finding all points in the curve...")
         for x_coord in range(self.p):
             self._process_x_coord_candidate(x_coord)
-            
+        print("Done\n")
+
+    def find_some_points(self, num_points_to_find):
+        for x in range(self.p):
+            self._process_x_coord_candidate(x)
+            if len(self.points) > num_points_to_find:
+                break
+        print(f"Found points: {self.points}\n")
+
     def _process_x_coord_candidate(self, x_coord):
         y_coord = self._try_find_y_coord(x_coord)
         if y_coord is not None:
@@ -24,29 +37,36 @@ class EllipticCurvePrime(object):
             # siguientes puntos
             point1 = EllipticPointPrime(x_coord, y_coord)
             point2 = EllipticPointPrime(x_coord, -y_coord % p)
-            self.points.add(point1)
-            self.points.add(point2)
+            self.points.append(point1)
+            self.points.append(point2)
         # Procesamos aparte el caso en que la 2a coordenada es 0
         self._process_0_y_coord(x_coord)
-     
+
     def _process_0_y_coord(self, x_coord):
         candidate_point = EllipticPointPrime(x_coord, 0)
-        if self.has_point(candidate_point):
-            self.points.add(candidate_point)
-        
+        if self.has_point(candidate_point, verbose=False):
+            self.points.append(candidate_point)
+
     def _right_side_fun(self, x):
         return (x ** 3 + self.A * x + B) % p
 
     def _try_find_y_coord(self, x_coord):
-        
         right_side = self._right_side_fun(x_coord)
         # modular_sqrt devuelve una raíz cuadrada si existe,
         # si no, devuelve 0
         sqrt_root = modular_sqrt(right_side, self.p)
         return sqrt_root
 
-    def has_point(self, point):
-        return (point.y ** 2) % p == self._right_side_fun(point.x)
+    def has_point(self, point, verbose=True):
+        if (point.y ** 2) % p == self._right_side_fun(point.x):
+            if verbose:
+                print(f"The point {point} belongs to {self}\n")
+            return True
+        else:
+            if verbose: 
+                print(f"The point {point} DOES NOT belong to {self}\n")
+            return False
+            
 
 
 class EllipticPointPrime(object):
@@ -67,7 +87,10 @@ class EllipticPointPrime(object):
     def __repr__(self):
         # Aquí decimos que tiene que hacer el programa cuando
         # escribimos print([un Point])
-        return f"({self.x}, {self.y})"
+        if self.is_infinite:
+            return "infinite"
+        else:
+            return f"({self.x}, {self.y})"
 
     def __eq__(self, other):
         # Aquí damos un criterio para que dos instancias de esta
@@ -102,26 +125,67 @@ class EllipticPointPrime(object):
         y3 = (lambda_ * (P1.x - x3) - P1.y) % p
         new_point = EllipticPointPrime(x3, y3)
         return new_point
+    
 
-    def mult_by_n(self, number):
-        new_point = EllipticPointPrime(self.x, self.y)
-        for _ in range(number-1):
-            new_point = new_point + new_point
-        return new_point
+def get_inverse_of_point(point):
+    return EllipticPointPrime(point.x, (-point.y)%p)
 
+def mult_point_by_num(point, number):
+    if number == 0:
+        return EllipticPointPrime(None, None)
+    new_point = EllipticPointPrime(point.x, point.y)
+    for _ in range(number-1):
+        new_point = new_point + point
+    return new_point
+
+
+def log(base, point):
+    # Buscamos  el ogaritmo del punto en base `base_point`
+    # por fuerza bruta
+    temp_point = EllipticPointPrime(base.x, base.y)
+    for num in range(1, p):
+        if temp_point == point:
+            print(f"The log of {point} in base {base} is {num}\n")
+            return num
+        temp_point = temp_point + base
+    print("Log not found\n")
 
 
 if __name__ == "__main__":
-    A = 10
-    B = 4
-    p = 191
+    # Parametros generales
+    p = 1046527
+    A = 0
+    B = 7
+    
+    # Declaración de la curva elíptica
     ec = EllipticCurvePrime(A, B, p)
-    ec.find_all_points()
-    print(ec.points)
-    P=EllipticPointPrime(43, 165)
-    has = ec.has_point(P)
-    print(has)
+    
+    # Esto imprimirá el el número de puntos de la curva elíptica que requiramos
+    ec.find_some_points(num_points_to_find=10)
 
-    Q = P.mult_by_n(10)
-    print(Q)
-    print(Q+Q)
+    # Esto crea el punto (60268, 35105)
+    P = EllipticPointPrime(60268, 35105)
+    # Y esto crea otro punto
+    Q = EllipticPointPrime(719386, 925426)
+
+    # Esto nos dice si P pertenece a la curvpa elíptica ec
+    ec.has_point(P)
+    # Lo mismo con Q
+    ec.has_point(Q)
+    
+    # Esto suma P y Q segun la operacion de la curva elíptica
+    # y llama R al resultado
+    R = P+Q
+    # El resultado debería estar en la curva:
+    ec.has_point(R)
+    
+    # Comprobando que `mult_point_by_num` funciona ok
+    assert P+P+P+P+P==mult_point_by_num(P,5)
+    
+    # Calculamos el log the P en base Q
+    log_num = log(Q, P)
+    
+    # Comprobando que la función `log` funciona ok
+    assert mult_point_by_num(Q,log_num) == P
+
+    print("Goodbye\n================================\n\n")
